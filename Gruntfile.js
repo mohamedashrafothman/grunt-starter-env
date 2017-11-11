36bbb138f5c94f640765053fbdc23b6845d07e2e
Mohamed A. Othman <mohamedashrafothman@gmail.com>
2017-11-11 18:55:58 +0200

    Version 1.0

---
+++ Gruntfile.js
@@ -0,0 +1,127 @@
+module.exports = function (grunt) {
+
+	// Project configuration.
+	grunt.initConfig({
+		concat: {
+			js: {
+				src: ['public/js/libs/*.js', 'public/js/*.js'],
+				dest: 'public/dist/script.js',
+			},
+			css: {
+				src: ['public/css/libs/*.css', 'public/css/*.css'],
+				dest: 'public/dist/style.css',
+			},
+		},
+		pug: {
+			compile: {
+				options: {
+					client: false,
+					pretty: true,
+					data: {
+						debug: false,
+					},
+				},
+				/**
+				 * - this files object takes the pug file dist and it's value is new html dist 
+				 *   ex: 'index.html':['public/views/index.pug']
+				 * - Repeat for each pug file your have.
+				 */
+				files: {
+					'index.html': ['public/views/index.pug'], 
+					'about.html': ['public/views/about.pug'], 
+					'404.html': ['public/views/404.pug'], 
+				}
+			},
+		},
+		sass: {
+			build: {
+				files: [
+					{
+						src: ['public/sass/style.scss'],
+						dest: 'public/css/style.css',
+					},
+				],
+			},
+		},
+		autoprefixer: {
+			options: {
+				browsers: ['last 2 versions', 'ie 8', 'ie 9'] 
+			},
+			my_target: {
+				files: [
+					{
+						src: 'public/css/style.css',
+						dest: 'public/css/style.css'
+					}
+				] 
+			}
+		},
+		cssmin: {
+			build: {
+				files: {
+					'public/dist/min/style.min.css': [
+						'public/dist/style.css',
+						'public/dist/!*.min.css',
+					],
+				},
+			},
+		},
+		uglify: {
+			my_target: {
+				files: [
+					{
+						src: ['public/dist/script.js'],
+						dest: 'public/dist/min/script.min.js',
+					},
+				]
+			},
+		},
+		eslint: {
+			all: ['public/js/*.js', '!node_modules/**/*.js']
+		},
+		imagemin: {
+			dynamic: {
+				files: [{
+					expand: true,
+					cwd: 'public/img/',
+					src: ['**/*.{png,jpg,jpeg,gif}'],
+					dest: 'public/img/'
+				}]
+			}
+		},
+		watch: {
+			sass: {
+				files: ['public/sass/**/*.scss'],
+				tasks: ['sass', 'autoprefixer', 'concat', 'cssmin'],
+			},
+			uglify: {
+				files: 'public/js/**/*.js',
+				tasks: ['eslint', 'concat', 'uglify'],
+			},
+			pug: {
+				files: ['public/views/**/*.pug'],
+				tasks: ['pug'],
+			},
+			imagemin: {
+				files: ['public/img/*.{png,jpeg,jpg,gif}'],
+				tasks: ['imagemin'],
+			}
+		},
+	});
+
+	
+	// Load the plugins that provides the tasks.
+	grunt.loadNpmTasks('grunt-contrib-imagemin');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-autoprefixer');
+	grunt.loadNpmTasks('grunt-contrib-pug');
+	grunt.loadNpmTasks('grunt-eslint');
+	grunt.loadNpmTasks('grunt-sass');
+
+
+	// Default task(s).
+	grunt.registerTask('default', ['watch']);
+};
---
+++ README.md
@@ -0,0 +1,32 @@
+# My Grunt Starter Config
+
+This is my project starting configuration setup which contains: 
+
+  - [SASS](https://github.com/sindresorhus/grunt-sass) Compiles sass files without using the compass ruby project.
+  - [ESLint](https://github.com/sindresorhus/grunt-eslint) Lints js code.
+  - [ImageMin](https://github.com/gruntjs/grunt-contrib-imagemin) Compresses images without affecting their quality.
+  - [CSSMin](https://github.com/gruntjs/grunt-contrib-cssmin) Compresses css.
+  - [Uglify](https://github.com/gruntjs/grunt-contrib-uglify) Compresses JavaScript.
+  - [Autoprefix](https://github.com/less/less-plugin-autoprefix) Prefix css code.
+  - [Concat](https://github.com/gruntjs/grunt-contrib-concat) Concatenates files in the order they are required.
+  - [Watch](https://github.com/gruntjs/grunt-contrib-watch) Watch all files changes after save.
+  - [PUG](https://github.com/gruntjs/grunt-contrib-pug) Compile Pug file into HTML5 files.
+
+### Installation
+
+1. Install [Node](https://nodejs.org/) and  [Grunt](http://gruntjs.com/getting-started) to get started
+
+2. Install the packages
+```sh
+npm install
+```
+
+3. running the watch file
+```sh
+npm start
+```
+
+### License
+----
+
+MIT
